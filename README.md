2d327b0ca83f8ee809796b7bb77dce6e52068ac1
Antony M <webdev101@protonmail.com>
2024-03-11 09:20:44 +0300

    implement search page

---
+++ README.md
@@ -0,0 +1,41 @@
+<br/>
+<div align="center">
+    <h3>PIXESCHAR</h3>
+    <p>Image search based on React, NextJS and Typescript</p>
+</div>
+
+## About this project
+pixeschar is an application meant to search the pixasbay datasource library. It is composed of two screens:
+1. The first screen is for searching the database
+2. The second screen is for displaying the details of the selected image
+
+### Built With
+This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+
+## Getting Started
+### Requirements
+* npm
+
+### Installation
+run ```npm install```
+
+Copy the *.env.example* file to *.env*.
+
+Edit your *.env* file and edit the **PIXASBAY_API_KEY** value with your pixasbay API key.
+
+## Usage
+Run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) to browse the application.
+
+Search for the image using the text box at the top and wait for the results in the image grid below. To view the details of a specific image in the grid, click on the image.
---
+++ src/app/page.tsx
@@ -0,0 +1,61 @@
+'use client'
+import { useState  } from "react";
+import Image from "next/image"
+
+export default function Home() {
+
+  const [pics, setPics] = useState<any[]>([]);
+  const [searchText, setSearchText] = useState('');
+  
+  function performSearch(){
+    console.log(`searching for '${searchText}'`)
+
+    if(encodeURIComponent(searchText).length > 100) {
+      console.error('Search query exceeds 100 characters')
+      return
+    }
+
+    let url = new URL(process.env.NEXT_PUBLIC_PIXASBAY_API_URL as string)
+    url.searchParams.append('key', process.env.NEXT_PUBLIC_PIXASBAY_API_KEY as string)
+    url.searchParams.append('q', searchText)
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        setPics(data.hits)
+      })
+      .catch(r => {
+        console.error('Error occured:' + r)
+      })
+  }
+
+  function updateSearchText(e: React.ChangeEvent<HTMLInputElement>){
+    setSearchText(e.target.value)
+  }
+
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <form className="w-full max-w-md mb-6">
+        <div className="flex items-center border-b border-teal-500 py-2">
+          <input value={searchText} onChange={updateSearchText} className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="What are you looking for?" aria-label="Full name"/>
+          <button className="flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded" type="button" onClick={performSearch}>
+            Search
+          </button>
+        </div>
+      </form>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {pics.map(function(pic, i){
+            return <div>
+              <img
+                  src={pic.previewURL}
+                alt={pic.tags}
+                width={pic.previewWidth}
+                height={pic.previewHeight}
+              />
+            </div>
+          })}
+      </div>
+    </main>
+  );
+}
